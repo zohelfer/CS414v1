322cf0aa1a2e4446b48237d15d7c2e1e5477a720
Niklaus Johnson <niklaus.johnson@gmail.com>
2014-10-09 10:00:19 -0600

    Represents the "system" (can be changed/deleted later)

---
+++ PizzaSystem/src/Registry.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Nik on 10/2/14.
+ */
+
+//This class is a bit conceptual right now.  We might end up deleting it later on.
+public class Registry { //Singleton class, represents the system information. This may also be a controller.
+
+    static int orderNumbers = 0;
+    static ArrayList<Order> currentOrders;
+    static ArrayList<Customer> customers;
+
+    private static Registry aRegistry;
+
+    private Registry() {
+        currentOrders = new ArrayList<Order>();
+        customers = new ArrayList<Customer>();
+    }
+
+    public static Registry getInstance() {
+        if (aRegistry == null) {
+            aRegistry = new Registry();
+        }
+
+        return aRegistry;
+    }
+
+    public static void addCustomer(Customer aCustomer) { //May need to refine this. Don't know if contains will do it correctly
+        if(!customers.contains(aCustomer)) {
+            customers.add(aCustomer);
+        }
+    }
+
+    public static void addOrder(Order anOrder) {
+        currentOrders.add(anOrder);
+    }
+
+    public static int getNewOrderNumber() {
+        orderNumbers = orderNumbers + 1;
+        return orderNumbers;
+    }
+
+    public static ArrayList<Order> getCurrentOrders() {
+        return currentOrders;
+    }
+
+    public static void setCurrentOrders(ArrayList<Order> currentOrders) {
+        Registry.currentOrders = currentOrders;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+}
